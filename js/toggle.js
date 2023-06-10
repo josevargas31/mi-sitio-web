5f04d142ff6796fd3caab1e619d27c54c0d0a116
josevargas31 <dajozavargas@gmail.com>
2023-06-10 18:14:25 -0500

    add fuentes

--- js/toggle.js
+++ js/toggle.js
@@ -1,16 +1,20 @@
-/* 1. Asociamos el ID del toggñe-box */
-let choiceBox = document.getElementById("choiceBox");
-let choiceBox = document.getElementById("choiceBox");
+/* 1. Asociamos el ID del toggle-box */
+let choiceBox = document.getElementById("choiceBox"); /* Circulo */
+let toggleBox = document.getElementById("toggleBox"); /* Rectangulo */
 /* Verificamos que la clase se encuentra en choice-box */
 choiceBox.addEventListener("click", function(){
   if (choiceBox.classList.contains("choiceBox")) {
     choiceBox.classList.add("choice-box-on")
-    choiceBox.classList.remove("choice-box-off")
+    choiceBox.classList.remove("choiceBox")
+    toggleBox.classList.add("toggle-box-on")
+    
   }
   else{
+    choiceBox.classList.add("choiceBox")
     choiceBox.classList.remove("choice-box-on")
-    choiceBox.classList.add("choice-box-off")
+    toggleBox.classList.remove("toggle-box-on")
   }
 })
+
 
 /* 2.1 Al dar clip quitamos la clase por defecto de choice-box y agregamos la clase que venia con el hover de choice-box */
