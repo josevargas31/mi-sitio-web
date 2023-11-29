7618c2620018aef7e9b18ede59aeb4cd9f8aa71f
jsoevargas31 <dajozavargas@gmail.com>
2023-11-29 16:44:59 -0500

    add amazon estrellas

--- js/amazon.js
+++ js/amazon.js
@@ -63,7 +63,16 @@ optionImagenes.addEventListener("click", function() {
   mainImgModal.style.display = "block";
   infoModal.style.display = "block";
 });
-
+/* Al pasar por encima de las estrellas aparecera calificaciones de las estrellas */
+let ocultEstrellas = document.getElementById("ocult-estrellas");
+optionVideos.addEventListener("", function() {
+  mainImgModal.style.display = "none";
+  infoModal.style.display = "none";                 /* Arreglar esto para que muestre calificaciones al pasar por encima de las estrellas */
+});
+optionImagenes.addEventListener("click", function() {
+  mainImgModal.style.display = "block";
+  infoModal.style.display = "block";
+});
 /*  */
 /*  */
 /* Cuando haga click en el "" se ejucutara el siguiente funcion */
@@ -206,7 +215,7 @@ let start2 = document.getElementById("start2")
 let start3 = document.getElementById("start3")
 let start4 = document.getElementById("start4")
 let start5 = document.getElementById("start5")
-let puntuacion = document.getElementById("puntuacion").innerText
+let puntuacion = document.getElementById("puntuacion").innerText = 3.2
     
     if(puntuacion >= 0 && puntuacion <= 0.2) {
         start1.setAttribute("class", "estrella fa-regular fa-star")
@@ -294,7 +303,7 @@ let puntuacion = document.getElementById("puntuacion").innerText
 /*  */
 /* zoom */
 // Para la lupa
-const imageContainer = document.querySelector('#producto');
+/* const imageContainer = document.querySelector('#producto');
 const magnifier = document.querySelector('.magnifier');
 const originalImage = document.getElementById('original-image');
 
@@ -308,7 +317,42 @@ imageContainer.addEventListener('mousemove', (e) => {
 
     magnifier.style.backgroundPosition = `${backgroundX}% ${backgroundY}%`;
     magnifier.style.display = 'block';
+}); */
+/*  */
+/*  */
+let puntuacion5 = document.getElementById("puntuacion5");
+let puntuacion4 = document.getElementById("puntuacion4");
+let puntuacion3 = document.getElementById("puntuacion3");
+let puntuacion2 = document.getElementById("puntuacion2");
+let puntuacion1 = document.getElementById("puntuacion1");
+
+puntuacion5.innerHTML = "13";
+puntuacion4.innerHTML = "33";
+puntuacion3.innerHTML = "33";
+puntuacion2.innerHTML = "33";
+puntuacion1.innerHTML = "33";
+
+let barra1Puntaje = document.getElementById("barra1Puntaje");
+let barra2Puntaje = document.getElementById("barra2Puntaje");
+let barra3Puntaje = document.getElementById("barra3Puntaje");
+let barra4Puntaje = document.getElementById("barra4Puntaje");
+let barra5Puntaje = document.getElementById("barra5Puntaje");
+
+let buttonGraficar = document.getElementById("buttonGraficar");
+
+buttonGraficar.addEventListener("click", function () {
+  // Calcula el total de votantes
+  let totalVotantes =
+    parseInt(puntuacion5.innerHTML) +
+    parseInt(puntuacion4.innerHTML) +
+    parseInt(puntuacion3.innerHTML) +
+    parseInt(puntuacion2.innerHTML) +
+    parseInt(puntuacion1.innerHTML);
+
+  // Calcula el porcentaje y actualiza la barra para cada puntuación
+  barra1Puntaje.style.width = (parseInt(puntuacion5.innerHTML) * 2) + "px";
+  barra2Puntaje.style.width = (parseInt(puntuacion4.innerHTML) * 2) + "px";
+  barra3Puntaje.style.width = (parseInt(puntuacion3.innerHTML) * 2) + "px";
+  barra4Puntaje.style.width = (parseInt(puntuacion2.innerHTML) * 2) + "px";
+  barra5Puntaje.style.width = (parseInt(puntuacion1.innerHTML) * 2) + "px";
 });
-
-
-
